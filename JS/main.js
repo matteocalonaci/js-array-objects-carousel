3b105a073d394ba8592f3b2277fd99ebd4ad8f53
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-05-07 15:38:08 +0200

    MILESTONE #0 Creazione del markup statico in HTML: - costruiamo il container e inseriamo l'immagine grande in modo da poter stilizzare lo slider.

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+// MILESTONE #0
+// Creazione del markup statico in HTML:
+// - costruiamo il container e inseriamo l'immagine grande in modo da poter stilizzare lo slider.
+
+// MILESTONE #1
+// Rimuoviamo i contenuti statici dall'HTML.
+// Costruiamo noi delle slide da aggiungere in pagina tramite un ciclo sul nostro array.
+
+// MILESTONE #2
+// Al click dell'utente sulle frecce verso sinistra o destra,
+// l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+
+// MILESTONE #3
+// Aggiungere il ciclo infinito del carosello.
+// Ovvero se l'immagine attiva è la prima e l'utente clicca la freccia verso destra,
+// la slide che deve attivarsi sarà l'ultima e viceversa per l'ultima slide se l'utente clicca la freccia verso sinistra.
